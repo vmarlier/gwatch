8c2d40b308f71b607b1dabc3a30a316dd22e69b4
Valentin Marlier <vmarlier@talend.com>
2021-12-12 20:40:30 +0100

    fix: box size when 2 boxes

--- internal/watch/screen.go
+++ internal/watch/screen.go
@@ -12,7 +12,7 @@ func DisplayOutput(output string, second int64) {
 	tm.Clear()
 
 	// Create Box with 30% width of current screen, and height of 20 lines
-	box := tm.NewBox(90|tm.PCT, 100|tm.PCT, 0)
+	box := tm.NewBox(96|tm.PCT, 100|tm.PCT, 0)
 
 	// Add some content to the box
 	// Note that you can add ANY content, even tables
@@ -30,24 +30,24 @@ func DisplayMultiplesOutput(output string, second int64) {
 	tm.Clear()
 
 	// Create Box with 30% width of current screen, and height of 20 lines
-	box := tm.NewBox(40|tm.PCT, 200|tm.PCT, 0)
+	box := tm.NewBox(48|tm.PCT, 100|tm.PCT, 0)
 
 	// Add some content to the box
 	// Note that you can add ANY content, even tables
 	fmt.Fprint(box, output)
 
 	// Move Box to approx center of the screen
-	tm.Print(tm.MoveTo(box.String(), 2|tm.PCT, 5|tm.PCT))
+	tm.Print(tm.MoveTo(box.String(), 2|tm.PCT, 2|tm.PCT))
 
 	// Create Box with 30% width of current screen, and height of 20 lines
-	box2 := tm.NewBox(40|tm.PCT, 200|tm.PCT, 0)
+	box2 := tm.NewBox(48|tm.PCT, 100|tm.PCT, 0)
 
 	// Add some content to the box
 	// Note that you can add ANY content, even tables
 	fmt.Fprint(box2, output)
 
 	// Move Box to approx center of the screen
-	tm.Print(tm.MoveTo(box2.String(), 52|tm.PCT, 5|tm.PCT))
+	tm.Print(tm.MoveTo(box2.String(), 52|tm.PCT, 2|tm.PCT))
 
 	tm.Flush()
 	time.Sleep(time.Second * time.Duration(second))
